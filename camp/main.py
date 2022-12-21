97c087590547d8ee71216917cb25aa67e4c25b28
GabrielWhite96 <105399927+GabrielWhite96@users.noreply.github.com>
2022-12-21 08:49:33 -0300

    update

--- camp/main.py
+++ camp/main.py
@@ -7,8 +7,15 @@ pygame.init()
 
 pygame.display.set_caption('Game')
 
+
 back_music = pygame.mixer.music.load('backMusic.mp3')
 pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.1)
+
+coli_sound = pygame.mixer.Sound('smb3_coin.wav')
+coli_sound.set_volume(0.1)
+
+background_image = pygame.image.load('2.png')
 
 width_screen = 1280
 heigh_screen = 720
@@ -18,21 +25,48 @@ screen = pygame.display.set_mode((width_screen, heigh_screen))
 # auxHeigh = True
 # auxY = 0
 
-movex = 600
-movey = 300
-orange_heigth = randint(50, 670)
-orange_width = randint(50, 1230)
+speed = 20
+x_control = speed
+y_control = 0
+x_snake = int(width_screen/2)
+y_snake = int(heigh_screen/2)
+x_apple = randint(50, 1230)
+y_apple = randint(50, 670)
 
 font = pygame.font.SysFont('arial', 40, True, False)
 points = 0
 
 clock = pygame.time.Clock()
 
+snake_length = 5
+
+died = False
+
+snake_list = []
+def add_snake(snake_list):
+  for XandY in snake_list:
+    pygame.draw.rect(screen, (0, 200, 50), (XandY[0],XandY[1] , 20, 20))
+
+def restart_game():
+  global points, snake_length, x_snake, y_snake, x_apple, y_apple, head_list, snake_list, died
+  points = 0
+  snake_length = 5
+  x_snake = int(width_screen/2)
+  y_snake = int(heigh_screen/2)
+  x_apple = randint(50, 1230)
+  y_apple = randint(50, 670)
+  head_list = []
+  snake_list = []
+  died = False
+
 while True:
   screen.fill((0, 0, 0))
-  clock.tick(60)
+  clock.tick(10)
+  
   msg_points = f'Pontos: {points}'
   formated_text = font.render(msg_points, True, (255, 255, 255))
+  
+  screen.blit(background_image, (0, 0))
 
   for event in pygame.event.get():
     if event.type == QUIT:
@@ -43,26 +77,42 @@ while True:
       if event.key == K_ESCAPE:
         pygame.quit()
         exit()
-    # if event.type == KEYDOWN:
-    #   if event.key == K_w:
-    #     movey = movey - 10
-    #   if event.key == K_a:
-    #     movex = movex - 10
-    #   if event.key == K_s:
-    #     movey = movey + 10
-    #   if event.key == K_d:
-    #     movex = movex + 10
+      if event.key == K_w:
+        if y_control == speed:
+          pass
+        else:
+          x_control = 0
+          y_control = -speed
+      if event.key == K_a:
+        if x_control == speed:
+          pass
+        else:
+          x_control = -speed
+          y_control = 0
+      if event.key == K_s:
+        if y_control == -speed:
+          pass
+        else:
+          x_control = 0
+          y_control = speed
+      if event.key == K_d:
+        if x_control == -speed:
+          pass
+        else:
+          x_control = speed
+          y_control = 0
     
-  if pygame.key.get_pressed()[K_w]:
-    movey = movey - 10
-  if pygame.key.get_pressed()[K_a]:
-    movex = movex - 10
-  if pygame.key.get_pressed()[K_s]:
-    movey = movey + 10
-  if pygame.key.get_pressed()[K_d]:
-    movex = movex + 10
-        
-        
+  x_snake = x_snake + x_control
+  y_snake = y_snake + y_control
+  # if pygame.key.get_pressed()[K_w]:
+  #   y_snake = y_snake - 10
+  # if pygame.key.get_pressed()[K_a]:
+  #   x_snake = x_snake - 10
+  # if pygame.key.get_pressed()[K_s]:
+  #   y_snake = y_snake + 10
+  # if pygame.key.get_pressed()[K_d]:
+  #   x_snake = x_snake + 10
+      
 
   # rect_orange = pygame.draw.rect(screen, (255, 100, 0), (200, 300, 50, 50))
   # circle_blue = pygame.draw.circle(screen, (0, 100, 255), (300, 325), 25)
@@ -84,14 +134,42 @@ while True:
   #   else:
   #     auxHeigh = True
   
-  rect_orange = pygame.draw.rect(screen, (255, 100, 0), (orange_width, orange_heigth, 50, 50))
-  rect_blue = pygame.draw.rect(screen, (0, 255, 150), (movex, movey, 50, 50))
+  head_list = []
+  head_list.append(x_snake)
+  head_list.append(y_snake)
   
-  if rect_orange.colliderect(rect_blue):
-    orange_heigth = randint(50, 670)
-    orange_width = randint(50, 1230)
+  snake_list.append(head_list)
+  add_snake(snake_list)
+  
+  apple = pygame.draw.rect(screen, (255, 0, 0), (x_apple, y_apple, 20, 20))
+  snake = pygame.draw.rect(screen, (0, 200, 50), (x_snake, y_snake, 20, 20))
+  
+  if len(snake_list) > snake_length:
+    del snake_list[0]
+  
+  if apple.colliderect(snake):
+    x_apple = randint(50, 1230)
+    y_apple = randint(50, 670)
     points = points + 1
+    coli_sound.play()
+    snake_length = snake_length + 2
+    
+  if snake_list.count(head_list) > 1:
+    died = True
+    while died:
+      screen.fill((50, 0, 0))
+      for event in pygame.event.get():
+        if event.type == QUIT:
+          pygame.quit()
+          exit()
+        if event.key == K_ESCAPE:
+          pygame.quit()
+          exit()
+        if event.type == KEYDOWN:
+          if event.key == K_r:
+            restart_game()
+      pygame.display.update()
   
   
-  screen.blit(formated_text, (1050, 30))
+  screen.blit(formated_text, (1030, 30))
   pygame.display.update()
